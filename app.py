53982b51cccd22b219471289a90d51eaacfc0dda
Raam Roch Hai <raam@raams-mbp.kpn>
2022-06-02 16:00:20 +0200

    removed dangling meta endpoint

--- app.py
+++ app.py
@@ -28,38 +28,12 @@ def startup_event():
     cuda_core=""
     mps_env = os.getenv("ENABLE_MPS")
 
-    if cuda_env is not None and cuda_env == "true" or cuda_env == "1":
-        cuda_support=True
-        cuda_core = os.getenv("CUDA_CORE")
-        if cuda_core is None or cuda_core == "":
-            cuda_core = "cuda:0"
-        logger.info(f"CUDA_CORE set to {cuda_core}")
-    if mps_env:
-        logger.info("Running on MPS")
-        mps_env = True
-    else:
-        logger.info("Running on CPU")
-
-    # Batch text tokenization enabled by default
-    direct_tokenize = False
-    transformers_direct_tokenize = os.getenv("T2V_TRANSFORMERS_DIRECT_TOKENIZE")
-    if transformers_direct_tokenize is not None and transformers_direct_tokenize == "true" or transformers_direct_tokenize == "1":
-        direct_tokenize = True
-
-    meta_config = Meta('./models/model')
-    vec = Vectorizer('./models/model', cuda_support, cuda_core, cuda_per_process_memory_fraction,
-                     meta_config.getModelType(), meta_config.get_architecture(), direct_tokenize, mps_env)
-
-
+    
 @app.get("/.well-known/live", response_class=Response)
 @app.get("/.well-known/ready", response_class=Response)
 async def live_and_ready(response: Response):
     response.status_code = status.HTTP_204_NO_CONTENT
 
-
-@app.get("/meta")
-def meta():
-    return meta_config.get()
 
 @app.post("/texts/") #future support for /medias/
 # Example Input JSON
